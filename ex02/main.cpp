a97f50716d6ad55f835eb3e253db55bba374f3b8
Riescent <github.herd7@aleeas.com>
2023-04-07 07:48:25 +0200

    Refactoring

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,11 +6,11 @@ int main(void) {
 	std::string*	stringPTR = &string;
 	std::string&	stringREF = string;
 
-	std::cout << "The memory address of the string variable: " << &string << std::endl
-			  << "The memory address held by stringPTR: " << stringPTR << std::endl
-			  << "The memory address held by stringREF: " << &stringREF << std::endl
-
-			  << "The value of the string variable: " << string << std::endl
-			  << "The value pointed to by stringPTR: " << *stringPTR << std::endl
+	std::cout << "The memory address of the string variable: " << &string << '\n'
+			  << "The memory address held by stringPTR: " << stringPTR << '\n'
+			  << "The memory address held by stringREF: " << &stringREF << '\n'
+			  << '\n'
+			  << "The value of the string variable: " << string << '\n'
+			  << "The value pointed to by stringPTR: " << *stringPTR << '\n'
 			  << "The value pointed to by stringREF: " << stringREF << std::endl;
 }
--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,69 +3,90 @@
 #include <cstdlib>
 #include <cstring>
 
-static int createInputFile(std::ifstream& inFile, char *inFileName);
-static int createOutputFile(std::ofstream& outFile, char *inFileName);
-static void writeToOutput(std::ifstream& inFile, std::ofstream& outFile,
-						  char **argv);
-static void printModifedLine(std::string& input, char **argv, size_t argv2Len,
-							 std::ofstream& outFile);
+static int openInputFile(std::ifstream& inputFile, const std::string& inputFileName);
+static int openOutputFile(std::ofstream& outputFile, const std::string& outputFileName);
+static void writeModifiedOutput(std::ifstream& inputFile, std::ofstream& outputFile,
+								const std::string& stringToReplace,
+								const std::string& stringToReplaceWith);
+static void printModifedLine(std::string& currentLine, const std::string& stringToReplace,
+							 const std::string& stringToReplaceWith,
+							 std::ofstream& outputFile);
 
-int main(int argc, char **argv) {
-	if (argc != 4) {
-		std::cerr << "Expected 3 arguments\n\t./sedIsForLosers FILE_NAME STR STR2"
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cerr << "Expected 3 arguments\n"
+				  << "\t./sedIsForLosers FILE_NAME STR_TO_REPLACE STR_TO_REPLACE_WITH"
 				  << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	std::ifstream inFile;
-	if (createInputFile(inFile, argv[1]) == EXIT_FAILURE)
+	const std::string inputFileName(argv[1]);
+	const std::string outputFileName(inputFileName + ".replace");
+	const std::string stringToReplace(argv[2]);
+	const std::string stringToReplaceWith(argv[3]);
+
+	std::ifstream inputFile;
+	if (openInputFile(inputFile, inputFileName) == EXIT_FAILURE)
 		return EXIT_FAILURE;
-	std::ofstream outFile;
-	if (createOutputFile(outFile, argv[1]) == EXIT_FAILURE)
+	std::ofstream outputFile;
+	if (openOutputFile(outputFile, outputFileName) == EXIT_FAILURE)
 		return EXIT_FAILURE;
 
-	writeToOutput(inFile, outFile, argv);
+	writeModifiedOutput(inputFile, outputFile, stringToReplace, stringToReplaceWith);
 }
 
-static int createInputFile(std::ifstream& inFile, char *inFileName) {
-	inFile.open(inFileName);
-	if (!inFile) {
-		std::cerr << "Failed to open " << inFileName << ": " << strerror(errno)
+static int openInputFile(std::ifstream& inputFile, const std::string& inputFileName)
+{
+	inputFile.open(inputFileName);
+	if (!inputFile)
+	{
+		std::cerr << "Failed to open " << inputFileName << ": " << strerror(errno)
 				  << std::endl;
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
 }
 
-static int createOutputFile(std::ofstream& outFile, char *inFileName) {
-	std::string outFileName = std::string(inFileName).append(".replace");
-	outFile.open(outFileName.c_str());
-	if (!outFile) {
-		std::cerr << "Failed to open " << outFileName << ": " << strerror(errno)
+static int openOutputFile(std::ofstream& outputFile, const std::string& outputFileName)
+{
+	outputFile.open(outputFileName);
+	if (!outputFile)
+	{
+		std::cerr << "Failed to open " << outputFileName << ": " << std::strerror(errno)
 				  << std::endl;
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
 }
 
-static void writeToOutput(std::ifstream& inFile, std::ofstream& outFile,
-						  char **argv) {
-	std::size_t argv2Len = std::strlen(argv[2]);
-	std::string input;
-	while (std::getline(inFile, input)) {
-		printModifedLine(input, argv, argv2Len, outFile);
-		if (!inFile.eof())
-			outFile << std::endl;
+static void writeModifiedOutput(std::ifstream& inputFile, std::ofstream& outputFile,
+								const std::string& stringToReplace,
+								const std::string& stringToReplaceWith)
+{
+	std::string currentLine;
+	while (std::getline(inputFile, currentLine))
+	{
+		printModifedLine(currentLine, stringToReplace, stringToReplaceWith, outputFile);
+		if (!inputFile.eof())
+			outputFile << std::endl;
 	}
 }
 
-static void printModifedLine(std::string& input, char **argv, size_t argv2Len,
-							 std::ofstream& outFile) {
-	std::size_t found = input.find(argv[2]);
-	while (found != std::string::npos) {
-		outFile << input.substr(0, found) << argv[3];
-		input = input.substr(found + argv2Len, input.length() - found - argv2Len);
-		found = input.find(argv[2]);
+static void printModifedLine(std::string& currentLine, const std::string& stringToReplace,
+							 const std::string& stringToReplaceWith,
+							 std::ofstream& outputFile)
+{
+	std::size_t found = currentLine.find(stringToReplace);
+	while (found != std::string::npos)
+	{
+		outputFile << currentLine.substr(0, found) << stringToReplaceWith;
+		const std::size_t newCurrentLineStartingIndex = found + stringToReplace.length();
+		const std::size_t newCurrentLineStringLen = currentLine.length() - found
+													- stringToReplace.length();
+		currentLine = currentLine.substr(newCurrentLineStartingIndex, newCurrentLineStringLen);
+		found = currentLine.find(stringToReplace);
 	}
-	outFile << input;
+	outputFile << currentLine;
 }
